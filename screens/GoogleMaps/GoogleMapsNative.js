d38a92f08c14864b40d6a880e14c4db7c7e61192
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-12-09 17:46:09 -0800

    map is rendered, but not functional

---
+++ screens/GoogleMaps/GoogleMapsNative.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+
+export default function GoogleMapsNative() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE}  />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
